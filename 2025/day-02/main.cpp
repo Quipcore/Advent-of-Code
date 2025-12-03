934f0c981b4c49baa06354047613af17c0277e81
Felix Lidö <felix.lido@gmail.com>
2025-12-04 00:57:44 +0100

    cleanup in 2025/day02

--- 2025/day-02/main.cpp
+++ 2025/day-02/main.cpp
@@ -4,7 +4,6 @@
 #include <functional>
 #include <iostream>
 #include <ostream>
-#include <set>
 #include <string>
 #include <vector>
 
@@ -14,48 +13,22 @@ bool part1(std::string str) {
          str.substr(halfStrLength, str.length());
 }
 
-std::vector<int> getFactors(int a) {
-  std::vector<int> factors;
-  for (int i = 2; i <= a; i++) {
-    if (a % i == 0) {
-      factors.push_back(i);
-    }
-  }
-
-  return factors;
-}
-
 bool part2(std::string str) {
-  std::vector<int> factors = getFactors(str.length());
+  for (int factor = 2; factor <= str.length(); factor++) {
+    if (str.length() % factor == 0) {
+      int lengthOfSegments = str.length() / factor;
 
-  for (int factor : factors) {
-    int lengthOfSegments = str.length() / factor;
-
-    // Check that all sub length are equal
-    std::set<std::string> set;
-    for (int n = 0; n < factor - 1; n++) {
-      int firstPos = lengthOfSegments * n;
-      int secondPos = lengthOfSegments * (n + 1);
-      std::string first = str.substr(firstPos, lengthOfSegments);
-      std::string second = str.substr(secondPos, lengthOfSegments);
-      set.insert(first);
-      set.insert(second);
-
-      //
-      // std::cout << "firstPos: " << firstPos << ", secondPos: " << secondPos
-      //           << ", len: " << lengthOfSegments << ", factor: " << factor
-      //           << ", first: " << first << ", second: "
-      //           << second
-      //           << std::endl;
-      //
-      assert(first.length() == second.length());
-      // if (!seqmentsAreEqual) {
-      //   continue;
-      // }
-    }
-
-    if (set.size() == 1) {
-      return true;
+      bool allSegmentsEqual = true;
+      for (int n = 0; n < factor - 1; n++) {
+        if (str.substr(lengthOfSegments * n, lengthOfSegments) !=
+            str.substr(lengthOfSegments * (n + 1), lengthOfSegments)) {
+          allSegmentsEqual = false;
+          break;
+        }
+      }
+      if (allSegmentsEqual) {
+        return true;
+      }
     }
   }
   return false;
