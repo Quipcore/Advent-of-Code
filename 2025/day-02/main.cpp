e4468ec70f61de2664613c2b8d94d05d722dbaa9
Felix Lidö <felix.lido@gmail.com>
2025-12-04 00:48:56 +0100

    solved 2025/day02

--- 2025/day-02/main.cpp
+++ 2025/day-02/main.cpp
@@ -1,17 +1,68 @@
 #include <algorithm>
+#include <cassert>
 #include <fstream>
+#include <functional>
 #include <iostream>
 #include <ostream>
+#include <set>
 #include <string>
 #include <vector>
 
-bool isInvalidID(std::string str) {
+bool part1(std::string str) {
   int halfStrLength = str.length() / 2;
   return str.substr(0, halfStrLength) ==
          str.substr(halfStrLength, str.length());
 }
 
-long long part1(std::vector<std::string> segments) {
+std::vector<int> getFactors(int a) {
+  std::vector<int> factors;
+  for (int i = 2; i <= a; i++) {
+    if (a % i == 0) {
+      factors.push_back(i);
+    }
+  }
+
+  return factors;
+}
+
+bool part2(std::string str) {
+  std::vector<int> factors = getFactors(str.length());
+
+  for (int factor : factors) {
+    int lengthOfSegments = str.length() / factor;
+
+    // Check that all sub length are equal
+    std::set<std::string> set;
+    for (int n = 0; n < factor - 1; n++) {
+      int firstPos = lengthOfSegments * n;
+      int secondPos = lengthOfSegments * (n + 1);
+      std::string first = str.substr(firstPos, lengthOfSegments);
+      std::string second = str.substr(secondPos, lengthOfSegments);
+      set.insert(first);
+      set.insert(second);
+
+      //
+      // std::cout << "firstPos: " << firstPos << ", secondPos: " << secondPos
+      //           << ", len: " << lengthOfSegments << ", factor: " << factor
+      //           << ", first: " << first << ", second: "
+      //           << second
+      //           << std::endl;
+      //
+      assert(first.length() == second.length());
+      // if (!seqmentsAreEqual) {
+      //   continue;
+      // }
+    }
+
+    if (set.size() == 1) {
+      return true;
+    }
+  }
+  return false;
+}
+
+long long summation(std::vector<std::string> segments,
+                    std::function<bool(std::string)> isInvalid) {
   long long total = 0;
 
   for (std::string range : segments) {
@@ -23,7 +74,7 @@ long long part1(std::vector<std::string> segments) {
     long long endPos = std::stoll(end);
 
     for (long long i = startPos; i <= endPos; i++) {
-      if (isInvalidID(std::to_string(i))) {
+      if (isInvalid(std::to_string(i))) {
         total += i;
       }
     }
@@ -49,6 +100,7 @@ int main(int argc, char *argv[]) {
   }
   file.close();
 
-  std::cout << "Result part1: " << part1(segments) << std::endl;
+  std::cout << "Result part1: " << summation(segments, &part1) << std::endl;
+  std::cout << "Result part2: " << summation(segments, &part2) << std::endl;
   return 0;
 }
